57a5b6f7dfc8eb2c3a97ad28dd934daf90317fac
Harsh Rai <74405723+harshrai654@users.noreply.github.com>
2022-09-24 19:35:21 +0530

    Add project form

--- components/Forms/AddProjectForm.tsx
+++ components/Forms/AddProjectForm.tsx
@@ -1,11 +1,21 @@
-import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import {
+	TextInput,
+	NumberInput,
+	Button,
+	Group,
+	Textarea,
+	SimpleGrid,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { IconCurrencyEthereum } from "@tabler/icons";
 
 export default function AddProjectForm() {
 	const form = useForm({
 		initialValues: {
 			title: "",
-			termsOfService: false,
+			description: "",
+			duration: 0,
+			budget: 0,
 		},
 
 		validate: {
@@ -13,19 +23,57 @@ export default function AddProjectForm() {
 				value.length > 3 ? null : "Enter a more descriptive title",
 		},
 	});
+
+	function handleAddProjectSubmit(formValues) {
+		console.log(formValues);
+	}
+
 	return (
-		<form onSubmit={form.onSubmit((values) => console.log(values))}>
+		<form
+			onSubmit={form.onSubmit((values) =>
+				handleAddProjectSubmit(values)
+			)}>
 			<TextInput
 				withAsterisk
 				label="Project Title"
+				radius="md"
+				required
 				{...form.getInputProps("email")}
 			/>
 
-			<Checkbox
-				mt="md"
-				label="I agree to sell my privacy"
-				{...form.getInputProps("termsOfService", { type: "checkbox" })}
+			<Textarea
+				placeholder="Enter Project Description"
+				label="Project Description"
+				withAsterisk
+				required
+				radius="md"
+				{...form.getInputProps("description")}
 			/>
+			<SimpleGrid cols={2}>
+				<NumberInput
+					placeholder="Duration"
+					label="Project Duration (Days)"
+					description="Expected number of days to complete the project"
+					radius="md"
+					withAsterisk
+					required
+					min={1}
+					{...form.getInputProps("duration")}
+				/>
+				<NumberInput
+					placeholder="Budget"
+					label="Project Budget"
+					radius="md"
+					step={0.0000001}
+					description="Expected Total Budget allocated"
+					precision={7}
+					required
+					min={0}
+					icon={<IconCurrencyEthereum size={18} />}
+					withAsterisk
+					{...form.getInputProps("budget")}
+				/>
+			</SimpleGrid>
 
 			<Group position="right" mt="md">
 				<Button type="submit">Submit</Button>
--- components/ModalWrapper.tsx
+++ components/ModalWrapper.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "@mantine/core";
+import { Modal, Title, Divider } from "@mantine/core";
 import { ModalContext } from "../contexts/ModalContext";
 import { useContext } from "react";
 
@@ -6,10 +6,19 @@ export default function ModalWrapper({ children, title }) {
 	const { open, setOpen } = useContext(ModalContext);
 	return (
 		<Modal
+			padding="xl"
 			opened={open}
 			onClose={() => setOpen(false)}
-			title={title}
-			fullScreen>
+			title={
+				<>
+					<Title>{title}</Title>
+					<Divider variant="dashed" />
+				</>
+			}
+			size="xl"
+			radius="lg"
+			overlayOpacity={0.8}
+			overlayBlur={5}>
 			{children}
 		</Modal>
 	);
